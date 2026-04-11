e9c8875d01bd8ae6abe2e3929e4f533cdf44eb60
skwzq <186171479+skwzq@users.noreply.github.com>
2026-04-11 20:50:23 +0300

    Parse input files with comments correctly

--- src/main.py
+++ src/main.py
@@ -6,24 +6,27 @@ from solver import Solver
 with open(argv[1], 'r') as file:
     input_ = file.read()
 
-input_ = input_.split()
+input_ = input_.splitlines()
 
-n_vars = int(input_[2])
+i = 0
+while i < len(input_):
+    if input_[i][0] == 'p':
+        break
+    i += 1
 
-f = [[]]
+n_vars = int(input_[i].split()[2])
 
-for i in range(4, len(input_)-1): # the last number is always 0
-    literal = int(input_[i])
-    if literal == 0:
-        f.append([])
-    else:
-        f[-1].append(literal)
+f = []
+
+for j in range(i+1, len(input_)):
+    clause = [int(x) for x in input_[j].split()[:-1]]
+    f.append(clause)
 
 
 solution = Solver(f, n_vars).solve()
 
 if solution:
     for x in solution:
-        print(x + ' ')
+        print(str(x) + ' ')
 else:
     print('unsatisfiable')
